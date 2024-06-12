390bbd22afe2b22d5913b75e435582f7eb4659e7
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-06-12 14:18:28 +0530

    chore: Add initial files and configurations for Angular Todo App

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  supabaseUrl: 'https://hwfoxavqaupdwgoiqeku.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3Zm94YXZxYXVwZHdnb2lxZWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMjU2OTMsImV4cCI6MjAzMzcwMTY5M30.bq-JtKeEATEIKhxe0HQlxJ-g-wZM77PaeD4Dq-ldx6I',
+};
